a91a674c784d7027a5b9d9a10aab85de65ed9aeb
deven <shriramsoft@gmail.com>
2020-02-24 19:19:59 +0530

    merge logic sheet 3

--- prc_WSGenerateRateTableDIDMerge.sql
+++ prc_WSGenerateRateTableDIDMerge.sql
@@ -32,6 +32,29 @@ BEGIN
 				primary key (ID)
 			);
 
+			DROP TEMPORARY TABLE IF EXISTS tmp_Components;
+			CREATE TEMPORARY TABLE tmp_Components (
+				ID int auto_increment,
+				Component VARCHAR(250)  ,
+				primary key (ID)
+			);
+
+            INSERT INTO tmp_Components(Component) VALUES
+										('OneOffCost'),
+										('MonthlyCost'),
+										('CostPerCall'),
+										('CostPerMinute'),
+										('SurchargePerCall'),
+										('SurchargePerMinute'),
+										('OutpaymentPerCall'),
+										('OutpaymentPerMinute'),
+										('Surcharges'),
+										('Chargeback'),
+										('CollectionCostAmount'),
+										('CollectionCostPercentage'),
+										('RegistrationCostPerNumber');
+
+
 
 
         /*
@@ -47,6 +70,12 @@ BEGIN
         INSERT INTO tmp_SelectedVendortblRateTableDIDRate
         select default as Timezones, max(OneOffCost) as OneOffCost , max(MonthlyCost) as MonthlyCost ...  from TMP_TABLE
         
+		Destination= Outpayment:
+			All outpayment components should be added
+			All other cost components should be deducted
+			Destination = Other cost component:
+			All outpayment components should be deducted
+			All other cost components should be added
 
 
         */
@@ -74,12 +103,19 @@ BEGIN
 			   ('all'			, 'MOB'			, 'MOB'		  , '0'				, 0		 	, 'max', 	 'all'		, 334				, 334		  , 'Premium Rate Number'  , 'Premium Rate Number',   '08998408'			, '08998408'		,     ''	, '1.22 per minute'   , ''		, '1.22 per minute');
 		*/
 
-		INSERT INTO `tmp_MergeComponents` 
+		/*INSERT INTO `tmp_MergeComponents` 
 			   (`Component`, `Origination`, `ToOrigination`, `TimezonesID`, `ToTimezonesID`, `Action`, `MergeTo`, `FromCountryID`, `ToCountryID`      , `FromAccessType`, 		`ToAccessType`, 		`FromPrefix` 		 , `ToPrefix`			, `FromCity`, `FromTariff`		, `ToCity`,	   `ToTariff`) 
 		VALUES ('all'			, 'FIX'			, 'FIX'		  , '0'				, 0		 	, 'max', 	 'all'		, 334				, 334		  , 'Premium Rate Number'  , 'Premium Rate Number',   '08998408'			, '08998408'		,     ''	, '1.22 per minute'   , ''		, '1.22 per minute'),
 			   ('all'			, 'MOB-H3G,MOB-Vodafone,MOB-Wind'			, 'MOB-Other'		  , '0'				, 0		 	, 'max', 	 'all'		, 334				, 334		  , 'Premium Rate Number'  , 'Premium Rate Number',   '08998408'			, '08998408'		,     ''	, '1.22 per minute'   , ''		, '1.22 per minute');
-
+		*/
 		
+		-- sheet 3
+		INSERT INTO `tmp_MergeComponents` 
+			   (`Component`, 										`Origination`, `ToOrigination`, `TimezonesID`, `ToTimezonesID`, `Action`, `MergeTo`, 			`FromCountryID`, `ToCountryID`      , `FromAccessType`, 		`ToAccessType`, 		`FromPrefix` 		 , `ToPrefix`			, `FromCity`, 		`FromTariff`		, `ToCity`,	   `ToTariff`) 
+		VALUES ('all'	,												 ''			, ''		  , '0'  			, 1			    , 'max',   'all'	, 				475			, 475		  		, 'Premium Rate Number'  , 'Premium Rate Number',   '0900'			, '0900'					,     ''			, '0.8 per minute'   , ''		, '0.8 per minute'),
+			   ('OutpaymentPerMinute,CostPerMinute,CollectionCostAmount', ''			, ''		  , '0'  			, 1			, 'max',  'OutpaymentPerMinute'	, 	475			, 475		  		, 'Premium Rate Number'  , 'Premium Rate Number',   '0900'			, '0900'					,     ''			, '0.8 per minute'   , ''		, '0.8 per minute');
+
+
 
 		ALTER TABLE tmp_SelectedVendortblRateTableDIDRate
 		ADD COLUMN OriginationCode2 VARCHAR(50) AFTER OriginationCode;
@@ -159,6 +195,29 @@ BEGIN
                 ELSEIF (@v_Component = 'ALL' AND @v_Action = 'sum' ) THEN
                 
                         SET @v_ResultField = 'SUM(OneOffCost) as OneOffCost ,SUM(MonthlyCost) as MonthlyCost ,SUM(CostPerCall) as CostPerCall ,SUM(CostPerMinute) as CostPerMinute ,SUM(SurchargePerCall) as SurchargePerCall ,SUM(SurchargePerMinute) as SurchargePerMinute ,SUM(OutpaymentPerCall) as OutpaymentPerCall ,SUM(OutpaymentPerMinute) as OutpaymentPerMinute ,SUM(Surcharges) as Surcharges ,SUM(Chargeback) as Chargeback ,SUM(CollectionCostAmount) as CollectionCostAmount ,SUM(CollectionCostPercentage) as CollectionCostPercentage ,SUM(RegistrationCostPerNumber) as RegistrationCostPerNumber ';
+				ELSE 
+
+
+					SET @v_ResultField = 'OneOffCost,MonthlyCost,CostPerCall,CostPerMinute,SurchargePerCall,SurchargePerMinute,OutpaymentPerCall,OutpaymentPerMinute,Surcharges,Chargeback,CollectionCostAmount,CollectionCostPercentage,RegistrationCostPerNumber';
+					SET @v_ResultField_2 = '';
+
+					IF @v_MergeTo like 'Outpayment%' THEN
+
+						set @v_ResultField_2 = REPLACE(@v_Component,',',' - ');
+						set @v_ResultField_2 = REPLACE(@v_ResultField_2,'- Outpayment','+ Outpayment');
+						
+					ELSE 
+
+						set @v_ResultField_2 = REPLACE(@v_Component,',',' + ');
+						set @v_ResultField_2 = REPLACE(@v_ResultField_2,'+ Outpayment','- Outpayment');
+
+
+					END IF ;
+
+					set @v_ResultField_2 = CONCAT('(' , @v_ResultField_2 , ') as ' , @v_MergeTo );
+
+					set @v_ResultField =  REPLACE(@v_ResultField, @v_MergeTo , @v_ResultField_2 );
+
 
                 END IF;
 
@@ -257,6 +316,7 @@ BEGIN
 				EXECUTE stm1;
 				DEALLOCATE PREPARE stm1;
 
+
 				-- delete source  records  
                 DELETE 
                 from tmp_SelectedVendortblRateTableDIDRate
@@ -284,96 +344,178 @@ BEGIN
  
  
 
-						insert into tmp_SelectedVendortblRateTableDIDRate
-						(
-										TimezonesID,
- 										CountryID,
-										AccessType,
- 										City,
-										Tariff,
-										Code,
-										OriginationCode,
-										OriginationCode2,
-										RateTableID,
-										CountryPrefix,
-										TimezoneTitle,
-										CodeDeckId,
-										VendorConnectionID ,
-										VendorID ,
-										EndDate ,
+				insert into tmp_SelectedVendortblRateTableDIDRate
+				(
+						TimezonesID,
+						CountryID,
+						AccessType,
+						City,
+						Tariff,
+						Code,
+						OriginationCode,
+						OriginationCode2,
+						RateTableID,
+						CountryPrefix,
+						TimezoneTitle,
+						CodeDeckId,
+						VendorConnectionID ,
+						VendorID ,
+						EndDate ,
 
-										OneOffCost,
-										MonthlyCost,
-										CostPerCall,
-										CostPerMinute,
-										SurchargePerCall,
-										SurchargePerMinute,
-										OutpaymentPerCall,
-										OutpaymentPerMinute,
-										Surcharges,
-										Chargeback,
-										CollectionCostAmount,
-										CollectionCostPercentage,
-										RegistrationCostPerNumber,
+						OneOffCost,
+						MonthlyCost,
+						CostPerCall,
+						CostPerMinute,
+						SurchargePerCall,
+						SurchargePerMinute,
+						OutpaymentPerCall,
+						OutpaymentPerMinute,
+						Surcharges,
+						Chargeback,
+						CollectionCostAmount,
+						CollectionCostPercentage,
+						RegistrationCostPerNumber,
 
-										OneOffCostCurrency ,
-										MonthlyCostCurrency ,
-										CostPerCallCurrency ,
-										CostPerMinuteCurrency ,
-										SurchargePerCallCurrency ,
-										SurchargePerMinuteCurrency ,
-										OutpaymentPerCallCurrency ,
-										OutpaymentPerMinuteCurrency ,
-										SurchargesCurrency ,
-										ChargebackCurrency ,
-										CollectionCostAmountCurrency ,
-										RegistrationCostPerNumberCurrency 
-						)
-						select
-										TimezonesID,
- 										CountryID,
-										AccessType,
- 										City,
-										Tariff,
-										Code,
-										OriginationCode,
-										OriginationCode2,
-										RateTableID,
-										CountryPrefix,
-										TimezoneTitle,
-										CodeDeckId,
-										VendorConnectionID ,
-										VendorID ,
-										EndDate ,
+						OneOffCostCurrency ,
+						MonthlyCostCurrency ,
+						CostPerCallCurrency ,
+						CostPerMinuteCurrency ,
+						SurchargePerCallCurrency ,
+						SurchargePerMinuteCurrency ,
+						OutpaymentPerCallCurrency ,
+						OutpaymentPerMinuteCurrency ,
+						SurchargesCurrency ,
+						ChargebackCurrency ,
+						CollectionCostAmountCurrency ,
+						RegistrationCostPerNumberCurrency 
+					)
+					select
+						TimezonesID,
+						CountryID,
+						AccessType,
+						City,
+						Tariff,
+						Code,
+						OriginationCode,
+						OriginationCode2,
+						RateTableID,
+						CountryPrefix,
+						TimezoneTitle,
+						CodeDeckId,
+						VendorConnectionID ,
+						VendorID ,
+						EndDate ,
 
-										OneOffCost,
-										MonthlyCost,
-										CostPerCall,
-										CostPerMinute,
-										SurchargePerCall,
-										SurchargePerMinute,
-										OutpaymentPerCall,
-										OutpaymentPerMinute,
-										Surcharges,
-										Chargeback,
-										CollectionCostAmount,
-										CollectionCostPercentage,
-										RegistrationCostPerNumber,
+						OneOffCost,
+						MonthlyCost,
+						CostPerCall,
+						CostPerMinute,
+						SurchargePerCall,
+						SurchargePerMinute,
+						OutpaymentPerCall,
+						OutpaymentPerMinute,
+						Surcharges,
+						Chargeback,
+						CollectionCostAmount,
+						CollectionCostPercentage,
+						RegistrationCostPerNumber,
 
-										OneOffCostCurrency ,
-										MonthlyCostCurrency ,
-										CostPerCallCurrency ,
-										CostPerMinuteCurrency ,
-										SurchargePerCallCurrency ,
-										SurchargePerMinuteCurrency ,
-										OutpaymentPerCallCurrency ,
-										OutpaymentPerMinuteCurrency ,
-										SurchargesCurrency ,
-										ChargebackCurrency ,
-										CollectionCostAmountCurrency ,
-										RegistrationCostPerNumberCurrency 
+						OneOffCostCurrency ,
+						MonthlyCostCurrency ,
+						CostPerCallCurrency ,
+						CostPerMinuteCurrency ,
+						SurchargePerCallCurrency ,
+						SurchargePerMinuteCurrency ,
+						OutpaymentPerCallCurrency ,
+						OutpaymentPerMinuteCurrency ,
+						SurchargesCurrency ,
+						ChargebackCurrency ,
+						CollectionCostAmountCurrency ,
+						RegistrationCostPerNumberCurrency 
 
-						from tmp_SelectedVendortblRateTableDIDRate_dup;
+					from tmp_SelectedVendortblRateTableDIDRate_dup;
+
+
+				-- CLEARE COMPONENT WHICH IS SELECTED IN RULE.
+
+				UPDATE tmp_SelectedVendortblRateTableDIDRate
+
+				SET	OneOffCost = CASE WHEN FIND_IN_SET('OneOffCost',@v_Component) != 0 AND 'OneOffCost'!= @v_MergeTo  THEN
+										0
+								 ELSE
+								 	OneOffCost
+								 END,
+					MonthlyCost = CASE WHEN FIND_IN_SET('MonthlyCost',@v_Component) != 0  AND 'MonthlyCost'!= @v_MergeTo  THEN
+										0
+								 ELSE
+								 	MonthlyCost
+								 END,
+					CostPerCall = CASE WHEN FIND_IN_SET('CostPerCall',@v_Component) != 0  AND 'CostPerCall'!= @v_MergeTo  THEN
+										0
+								 ELSE
+								 	CostPerCall
+								 END,
+					CostPerMinute = CASE WHEN FIND_IN_SET('CostPerMinute',@v_Component) != 0  AND 'CostPerMinute'!= @v_MergeTo  THEN
+										0
+								 ELSE
+								 	CostPerMinute
+								 END,
+					SurchargePerCall = CASE WHEN FIND_IN_SET('SurchargePerCall',@v_Component) != 0  AND 'SurchargePerCall'!= @v_MergeTo  THEN
+										0
+								 ELSE
+								 	SurchargePerCall
+								 END,
+					SurchargePerMinute = CASE WHEN FIND_IN_SET('SurchargePerMinute',@v_Component) != 0  AND 'SurchargePerMinute'!= @v_MergeTo  THEN
+										0
+								 ELSE
+								 	SurchargePerMinute
+								 END,
+					OutpaymentPerCall = CASE WHEN FIND_IN_SET('OutpaymentPerCall',@v_Component) != 0  AND 'OutpaymentPerCall'!= @v_MergeTo  THEN
+										0
+								 ELSE
+								 	OutpaymentPerCall
+								 END,
+					OutpaymentPerMinute = CASE WHEN FIND_IN_SET('OutpaymentPerMinute',@v_Component) != 0  AND 'OutpaymentPerMinute'!= @v_MergeTo  THEN
+										0
+								 ELSE
+								 	OutpaymentPerMinute
+								 END,
+					Surcharges = CASE WHEN FIND_IN_SET('Surcharges',@v_Component) != 0  AND 'Surcharges'!= @v_MergeTo  THEN
+										0
+								 ELSE
+								 	Surcharges
+								 END,
+					Chargeback = CASE WHEN FIND_IN_SET('Chargeback',@v_Component) != 0  AND 'Chargeback'!= @v_MergeTo  THEN
+										0
+								 ELSE
+								 	Chargeback
+								 END,
+					CollectionCostAmount = CASE WHEN FIND_IN_SET('CollectionCostAmount',@v_Component) != 0  AND 'CollectionCostAmount'!= @v_MergeTo  THEN
+										0
+								 ELSE
+								 	CollectionCostAmount
+								 END,
+					CollectionCostPercentage = CASE WHEN FIND_IN_SET('CollectionCostPercentage',@v_Component) != 0  AND 'CollectionCostPercentage'!= @v_MergeTo  THEN
+										0
+								 ELSE
+								 	CollectionCostPercentage
+								 END,
+					RegistrationCostPerNumber = CASE WHEN FIND_IN_SET('RegistrationCostPerNumber',@v_Component) != 0  AND 'OneOffCost'!= @v_MergeTo  THEN
+										0
+								 ELSE
+								 	RegistrationCostPerNumber
+								 END
+						
+	                where 
+                        (  fn_IsEmpty(@p_Timezone) 		OR  FIND_IN_SET(TimezonesID,@p_Timezone) != 0 )
+                    AND (  fn_IsEmpty(@p_Origination)  	 OR FIND_IN_SET(OriginationCode,@p_Origination) != 0 OR FIND_IN_SET(OriginationCode2,@p_Origination) != 0)
+                    AND (  fn_IsEmpty(@v_FromCountryID) OR CountryID = 	@v_FromCountryID )
+                    AND (  fn_IsEmpty(@v_FromAccessType) OR AccessType = 	@v_FromAccessType )
+                    AND (  fn_IsEmpty(@v_FromPrefix)  OR Code = 	concat(CountryPrefix ,@v_FromPrefix) )
+                    AND (  fn_IsEmpty(@v_FromCity)  OR City = 	@v_FromCity )
+                    AND (  fn_IsEmpty(@v_FromTariff)  OR Tariff = 	@v_FromTariff );
+			 
+								  
 
 
  
